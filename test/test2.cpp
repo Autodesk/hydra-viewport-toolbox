80a083ac02848e2ae8b86ae9005d5a2aaefc2ca7
Patrick Hodoul <patrick.hodoul@autodesk.com>
2025-04-16 18:26:43 -0400

    glew as static lib
    Signed-off-by: Patrick Hodoul <patrick.hodoul@autodesk.com>

--- test/test2.cpp
+++ test/test2.cpp
@@ -78,11 +78,7 @@ TEST(test3, BasicAssertions)
 
     ASSERT_TRUE(initGlew());
 
-#if defined(__APPLE__)
-    auto hgi = pxr::Hgi::CreateNamedHgi(pxr::HgiTokens->Metal);
-#else
-    auto hgi = pxr::Hgi::CreateNamedHgi(pxr::HgiTokens->OpenGL);
-#endif
+    auto hgi = pxr::Hgi::CreatePlatformDefaultHgi();
     ASSERT_TRUE(hgi);
     ASSERT_TRUE(hgi->IsBackendSupported());
 
