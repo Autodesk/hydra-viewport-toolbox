a68d30c9ff5bdb847d3991c39b788b2382519ced
Patrick Hodoul <Patrick.Hodoul@autodesk.com>
2025-04-24 19:08:39 -0400

    OGSMOD-7044: Add the Unit Test Framework, part I (#38)
    Signed-off-by: Patrick Hodoul <patrick.hodoul@autodesk.com>
    Signed-off-by: Patrick Hodoul <Patrick.Hodoul@autodesk.com>
    Co-authored-by: Laurence Laforest <laurence.laforest@autodesk.com>

---
+++ test/main.cpp
@@ -0,0 +1,57 @@
+// Copyright 2025 Autodesk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    auto glfw_error_callback = [](int error, const char* description)
+    {
+        throw std::runtime_error(std::string("GLFW error: code ") + std::to_string(error) + ": " +
+            (description ? description : ""));
+    };
+    glfwSetErrorCallback(glfw_error_callback);
+
+    if (glfwInit() == GLFW_FALSE)
+    {
+        const char* description;
+        glfwGetError(&description);
+        std::cerr << "GLFW initialization failed:" << description << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    int ret = EXIT_FAILURE;
+    try
+    {
+        ret = RUN_ALL_TESTS();
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << "Unexpected failure: " << ex.what() << std::endl;
+    }
+    catch (...)
+    {
+        std::cerr << "Unexpected failure" << std::endl;
+    }
+
+    glfwTerminate();
+
+    return ret;
+}
