40ee6153a95a35356b79d08717375bc649cf01c2
Erika Harrison <erika.harrison@autodesk.com>
2025-05-01 14:34:50 -0600

    OGSMOD-7006: Use local resources (#40)
    Co-authored-by: Allan Rocha <allan.rocha@autodesk.com>

--- source/tasks/resources.cpp
+++ source/tasks/resources.cpp
@@ -45,13 +45,13 @@ const std::filesystem::path GetResourceDirectory()
 const std::filesystem::path GetGizmoPath(std::string const& gizmoFile)
 {
     auto resourceDir = GetResourceDirectory();
-    return resourceDir.append("Gizmos").append(gizmoFile);
+    return resourceDir.append("gizmos").append(gizmoFile);
 }
 
 const std::filesystem::path GetShaderPath(std::string const& shaderFile)
 {
     auto resourceDir = GetResourceDirectory();
-    return resourceDir.append("Shaders").append(shaderFile);
+    return resourceDir.append("shaders").append(shaderFile);
 }
 
 } // namespace HVT_NS
