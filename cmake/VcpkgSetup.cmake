ae22a8ae6c3703bf1b5a572d0efb087291f88fd3
Aleksi Sapon <aleksi.sapon@autodesk.com>
2025-05-26 06:56:56 -0400

    vcpkg USD port fixes (#72)

--- cmake/VcpkgSetup.cmake
+++ cmake/VcpkgSetup.cmake
@@ -12,44 +12,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# We can't use PROJECT_IS_TOP_LEVEL because it's
+# empty until the first project() call.
+if(PROJECT_SOURCE_DIR)
+    message(STATUS "Skipping vcpkg when used in another project")
+    return()
+endif()
 
 # Features to enable in vcpkg prior to the project definition.
 if(ENABLE_TESTS)
   list(APPEND VCPKG_MANIFEST_FEATURES "tests")
 endif()
 # Enable vcpkg USD feature only if no local USD path is provided
-if(NOT DEFINED OPENUSD_INSTALL_PATH)
-  list(APPEND VCPKG_MANIFEST_FEATURES "usd-build")
+if(NOT OPENUSD_INSTALL_PATH)
+  list(APPEND VCPKG_MANIFEST_FEATURES "usd-minimal")
 endif()
 
-# VCPKG Bootstrap prior to the project definition.
-if(VCPKG_MANIFEST_FEATURES)
-    set(VCPKG_DIR "${CMAKE_SOURCE_DIR}/vcpkg")   
-    if(NOT EXISTS "${VCPKG_DIR}")
-        message(STATUS "vcpkg directory not found but vcpkg features were requested. Fetching submodules.")
-        execute_process(COMMAND git submodule update --init --recursive
-            WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}")
+set(vcpkg_dir "${CMAKE_SOURCE_DIR}/vcpkg")
+if(NOT EXISTS "${vcpkg_dir}/ports")
+    message(STATUS "Initializing vcpkg submodule...")
+    execute_process(COMMAND git submodule update --init --recursive
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}" COMMAND_ERROR_IS_FATAL ANY)
+endif()
+
+# vcpkg executable is present if boostrap was already executed
+if(WIN32)
+    if(NOT EXISTS "${vcpkg_dir}/vcpkg.exe")
+        message(STATUS "Bootstrapping vcpkg...")
+        execute_process(COMMAND cmd /c bootstrap-vcpkg.bat
+            WORKING_DIRECTORY "${vcpkg_dir}" COMMAND_ERROR_IS_FATAL ANY)
     endif()
-    # vcpkg executable is present if boostrap was already executed
-    if(WIN32)
-        if(NOT EXISTS "${VCPKG_DIR}/vcpkg.exe")
-            message(STATUS "Bootstrapping vcpkg (Windows)...")
-            execute_process(COMMAND cmd /c bootstrap-vcpkg.bat
-                WORKING_DIRECTORY "${VCPKG_DIR}")
-        endif()
-    else()
-        if(NOT EXISTS "${VCPKG_DIR}/vcpkg")
-            message(STATUS "Bootstrapping vcpkg (Unix)...")
-            execute_process(COMMAND ./bootstrap-vcpkg.sh
-                WORKING_DIRECTORY "${VCPKG_DIR}")
-        endif()
-    endif()
-    # Define the toolchain if it was not already defined in the command line
-    if(NOT DEFINED CMAKE_TOOLCHAIN_FILE)
-        set(CMAKE_TOOLCHAIN_FILE "${VCPKG_DIR}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "")
-    endif()
-    # Overlay ports are there to change the default behavior of vcpkg on standard ports or add new ports.
-    set(VCPKG_OVERLAY_PORTS "${CMAKE_SOURCE_DIR}/overlay-ports")
 else()
-    message(STATUS "Not using vcpkg for hydra-viewport-toolbox because no dependencies requested...")
+    if(NOT EXISTS "${vcpkg_dir}/vcpkg")
+        message(STATUS "Bootstrapping vcpkg...")
+        execute_process(COMMAND ./bootstrap-vcpkg.sh
+            WORKING_DIRECTORY "${vcpkg_dir}" COMMAND_ERROR_IS_FATAL ANY)
+    endif()
+endif()
+
+# Define the toolchain if it was not already defined in the command line
+if(NOT CMAKE_TOOLCHAIN_FILE)
+    set(CMAKE_TOOLCHAIN_FILE "${vcpkg_dir}/scripts/buildsystems/vcpkg.cmake")
 endif()
