5a6d761005cbe15f5499a9cb482c2a5c8b20ad45
Jackson Sun <Jackson.Sun@autodesk.com>
2025-07-21 23:10:27 -0400

    Add API updateMaterial for material property change

--- source/dataSource/dataSource.cpp
+++ source/dataSource/dataSource.cpp
@@ -70,6 +70,12 @@ bool SceneDataSource::bindMaterial(const SdfPath& /* primPath */, const VtValue&
     return false;
 }
 
+bool SceneDataSource::updateMaterial(const SdfPath& /*matPrimPath*/, const TfToken& /*prop*/, const VtValue& /*newPropValue*/)
+{
+    TF_RUNTIME_ERROR("No implementation for SceneDataSource::updateMaterial()");
+    return false;
+}
+
 bool SceneDataSource::isPrimitive(const SdfPath& /* path */) const
 {
     return false;
