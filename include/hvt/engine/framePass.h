7ae436cb4dcf87f06fd937253091fdeda0c45953
Patrick Hodoul <Patrick.Hodoul@autodesk.com>
2025-08-13 18:29:06 -0400

    Update framePass.h
    The pull request updates the current comments around the color & depth buffer attributes.

--- include/hvt/engine/framePass.h
+++ include/hvt/engine/framePass.h
@@ -122,16 +122,18 @@ struct HVT_API FramePassParams : public BasicLayerParams
     ModelParams modelInfo;
     /// @}
 
-    /// Color settings.
-    /// @{
+    /// Enable the color correction task if present.
     bool enableColorCorrection { true };
-    PXR_NS::GfVec4f backgroundColor { 0.025f, 0.025f, 0.025f, 1.0f };
-    float backgroundDepth {1.0f};
+
     /// Clear the background of the color buffer.
     bool clearBackgroundColor{ true };
+    /// The color to use when clearing the color buffer.
+    PXR_NS::GfVec4f backgroundColor { 0.025f, 0.025f, 0.025f, 1.0f };
+
     /// Clear the background of the depth buffer.
     bool clearBackgroundDepth { false };
-    /// @}
+    /// The color to use when clearing the depth buffer.
+    float backgroundDepth { 1.0f };
 
     /// MSAA settings.
     /// @{
