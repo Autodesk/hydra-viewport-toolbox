5b62a0896e9c61325c19dd81585f5563a30d52dc
Patrick Hodoul <patrick.hodoul@autodesk.com>
2026-02-06 11:27:21 -0500

    OGSMOD-8424 - Investigate slowdown with three frame passes
    Signed-off-by: Patrick Hodoul <patrick.hodoul@autodesk.com>

---
+++ source/engine/engine.cpp
@@ -0,0 +1,245 @@
+// Copyright 2025 Autodesk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <hvt/engine/engine.h>
+
+// clang-format off
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
+#elif defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable : 4003)
+#endif
+// clang-format on
+
+#include <pxr/base/tf/diagnostic.h>
+#include <pxr/base/trace/trace.h>
+#include <pxr/imaging/hd/debugCodes.h>
+#include <pxr/imaging/hd/perfLog.h>
+#include <pxr/imaging/hd/renderDelegate.h>
+#include <pxr/imaging/hd/renderIndex.h>
+#include <pxr/imaging/hd/tokens.h>
+
+// clang-format off
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#elif defined(_MSC_VER)
+#pragma warning(pop)
+#endif
+// clang-format on
+
+PXR_NAMESPACE_USING_DIRECTIVE
+
+namespace HVT_NS
+{
+
+Engine::Engine() : _taskContext() {}
+
+Engine::~Engine() = default;
+
+void Engine::SetTaskContextData(TfToken const& id, VtValue const& data)
+{
+    // See if the token exists in the context and if not add it.
+    std::pair<HdTaskContext::iterator, bool> result = _taskContext.emplace(id, data);
+    if (!result.second)
+    {
+        // Item wasn't new, so need to update it
+        result.first->second = data;
+    }
+}
+
+bool Engine::GetTaskContextData(TfToken const& id, VtValue* data) const
+{
+    if (!TF_VERIFY(data, "data is nullptr"))
+    {
+        return false;
+    }
+
+    auto const& it = _taskContext.find(id);
+    if (it != _taskContext.end())
+    {
+        *data = it->second;
+        return true;
+    }
+
+    return false;
+}
+
+void Engine::RemoveTaskContextData(TfToken const& id)
+{
+    _taskContext.erase(id);
+}
+
+void Engine::ClearTaskContextData()
+{
+    _taskContext.clear();
+}
+
+void Engine::Execute(HdRenderIndex* index, HdTaskSharedPtrVector* tasks)
+{
+    TRACE_FUNCTION();
+
+    if ((index == nullptr) || (tasks == nullptr))
+    {
+        TF_CODING_ERROR("Passed nullptr to Engine::Execute()");
+        return;
+    }
+
+    // Some render tasks may need access to the same rendering context / driver
+    // as the render delegate. For example some tasks use Hgi.
+    _taskContext[HdTokens->drivers] = VtValue(index->GetDrivers());
+
+    // --------------------------------------------------------------------- //
+    // DATA DISCOVERY PHASE
+    // --------------------------------------------------------------------- //
+    // Discover all required input data needed to render the required render
+    // prim representations. At this point, we must read enough data to
+    // establish the resource dependency graph, but we do not yet populate CPU-
+    // or GPU-memory with data.
+
+    // As a result of the next call, the resource registry will be populated
+    // with both BufferSources that need to be resolved (possibly generating
+    // data on the CPU) and computations to run on the CPU/GPU.
+
+    TF_DEBUG(HD_ENGINE_PHASE_INFO)
+        .Msg("\n"
+             "==============================================================\n"
+             "  Engine [Data Discovery Phase](RenderIndex::SyncAll)         \n"
+             "--------------------------------------------------------------\n");
+    {
+        TRACE_FUNCTION_SCOPE("Data Discovery");
+        index->SyncAll(tasks, &_taskContext);
+    }
+
+    const size_t numTasks = tasks->size();
+
+    // --------------------------------------------------------------------- //
+    // PREPARE PHASE
+    // --------------------------------------------------------------------- //
+    // Now that all Prims have obtained obtained their current states
+    // we can now prepare the task system for rendering.
+    //
+    // While sync operations are change-tracked, so are only performed if
+    // something is dirty, prepare operations are done for every execution.
+    //
+    // As tasks are synced first, they cannot resolve their bindings at sync
+    // time, so this is where tasks perform their inter-prim communication.
+    //
+    // The prepare phase is also where a task manages the resources it needs
+    // for the render phase.
+    TF_DEBUG(HD_ENGINE_PHASE_INFO)
+        .Msg("\n"
+             "==============================================================\n"
+             "  Engine [Prepare Phase](Task::Prepare)                       \n"
+             "--------------------------------------------------------------\n");
+    {
+        TRACE_FUNCTION_SCOPE("Task Prepare");
+        for (size_t taskNum = 0; taskNum < numTasks; ++taskNum)
+        {
+            const HdTaskSharedPtr& task = (*tasks)[taskNum];
+
+            task->Prepare(&_taskContext, index);
+        }
+    }
+
+    // --------------------------------------------------------------------- //
+    // DATA COMMIT PHASE
+    // --------------------------------------------------------------------- //
+    // Having acquired handles to the data needed to update various resources,
+    // we let the render delegate 'commit' these resources. These resources may
+    // reside either on the CPU/GPU/both; that depends on the render delegate
+    // implementation.
+    TF_DEBUG(HD_ENGINE_PHASE_INFO)
+        .Msg("\n"
+             "==============================================================\n"
+             "  Engine [Data Commit Phase](RenderDelegate::CommitResources) \n"
+             "--------------------------------------------------------------\n");
+    HdRenderDelegate* renderDelegate = index->GetRenderDelegate();
+    {
+        TRACE_FUNCTION_SCOPE("Data Commit");
+        renderDelegate->CommitResources(&index->GetChangeTracker());
+    }
+
+    // --------------------------------------------------------------------- //
+    // EXECUTE PHASE
+    // --------------------------------------------------------------------- //
+    // Having updated all the necessary data buffers, we can finally execute
+    // the rendering tasks.
+    TF_DEBUG(HD_ENGINE_PHASE_INFO)
+    .Msg("\n"
+         "==============================================================\n"
+         "  Engine [Execute Phase](Task::Execute)                       \n"
+         "--------------------------------------------------------------\n");
+    {
+        TRACE_FUNCTION_SCOPE("Task Execution");
+
+        for (size_t taskNum = 0; taskNum < numTasks; ++taskNum)
+        {
+            const HdTaskSharedPtr& task = (*tasks)[taskNum];
+
+            task->Execute(&_taskContext);
+        }
+    }
+}
+
+void Engine::Execute(HdRenderIndex* const index, SdfPathVector const& taskPaths)
+{
+    HdTaskSharedPtrVector tasks;
+    tasks.reserve(taskPaths.size());
+    for (SdfPath const& taskPath : taskPaths)
+    {
+        if (taskPath.IsEmpty())
+        {
+            TF_CODING_ERROR("Empty task path given to Engine::Execute()");
+            continue;
+        }
+        HdTaskSharedPtr task = index->GetTask(taskPath);
+        if (!task)
+        {
+            TF_CODING_ERROR(
+                "No task at %s in render index in Engine::Execute()", taskPath.GetText());
+            continue;
+        }
+        tasks.push_back(std::move(task));
+    }
+    Execute(index, &tasks);
+}
+
+bool Engine::AreTasksConverged(HdRenderIndex* const index, SdfPathVector const& taskPaths)
+{
+    for (SdfPath const& taskPath : taskPaths)
+    {
+        if (taskPath.IsEmpty())
+        {
+            TF_CODING_ERROR("Empty task path given to Engine::AreTasksConverged()");
+            continue;
+        }
+        HdTaskSharedPtr const task = index->GetTask(taskPath);
+        if (!task)
+        {
+            TF_CODING_ERROR(
+                "No task at %s in render index in Engine::AreTasksConverged()", taskPath.GetText());
+            continue;
+        }
+        if (!task->IsConverged())
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+} // namespace HVT_NS
---
+++ test/tests/testThreeFramePasses.cpp
@@ -0,0 +1,336 @@
+// Copyright 2026 Autodesk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifdef __APPLE__
+#include "TargetConditionals.h"
+#endif
+
+#include "composeTaskHelpers.h"
+
+#include <RenderingFramework/TestContextCreator.h>
+#include <RenderingFramework/TestFlags.h>
+#include <RenderingUtils/CollectTraces.h>
+
+#include <hvt/engine/viewportEngine.h>
+#include <hvt/tasks/blurTask.h>
+
+#include <pxr/imaging/hd/mergingSceneIndex.h>
+#include <pxr/imaging/hd/renderDelegate.h>
+#include <pxr/imaging/hd/tokens.h>
+#include <pxr/usd/usdGeom/xformable.h>
+
+#include <gtest/gtest.h>
+
+#include <pxr/pxr.h>
+
+PXR_NAMESPACE_USING_DIRECTIVE
+
+HVT_TEST(TestViewportToolbox, TestThreeFramePasses)
+{
+    RenderingUtils::CollectTraces collectTraces;
+
+    // This unit test validates rendering with three frame passes with three render calls.
+
+    auto context = TestHelpers::CreateTestContext();
+
+    TestHelpers::TestStage stage(context->_backend);
+
+    auto filepath =
+        (TestHelpers::getAssetsDataFolder() / "usd" / "default_scene.usdz").generic_u8string();
+    ASSERT_TRUE(stage.open(filepath));
+
+    TestHelpers::FramePassInstance framePass1, framePass2, framePass3;
+    HdMergingSceneIndexRefPtr mergingSceneIndex;
+
+    // Create two scene indices and merge them for the first two frame passes.
+
+    {
+        auto sceneStage1 = hvt::ViewportEngine::CreateStageFromFile(filepath);
+        auto sceneIndex1 = hvt::ViewportEngine::CreateUSDSceneIndex(sceneStage1);
+
+        auto sceneStage2 = hvt::ViewportEngine::CreateStageFromFile(context->_sceneFilepath);
+        {
+            // Get the root prim from scene stage.
+            UsdPrim rootPrim = sceneStage2->GetPrimAtPath(SdfPath("/mesh_0"));
+
+            // Add a zoom (scale transform) to the root prim.
+            UsdGeomXformable xformable(rootPrim);
+            if (xformable)
+            {
+                static constexpr double zoomFactor = 20.0;
+                GfVec3d scale(zoomFactor, zoomFactor, zoomFactor);
+
+                UsdGeomXformOp scaleOp = xformable.AddScaleOp(UsdGeomXformOp::PrecisionDouble);
+                scaleOp.Set(scale);
+            }
+        }
+        auto sceneIndex2 = hvt::ViewportEngine::CreateUSDSceneIndex(sceneStage2);
+
+        // Merge the scene indices.
+        mergingSceneIndex = HdMergingSceneIndex::New();
+        mergingSceneIndex->AddInputScene(sceneIndex1, SdfPath::AbsoluteRootPath());
+        mergingSceneIndex->AddInputScene(sceneIndex2, SdfPath::AbsoluteRootPath());
+    }
+
+    // Create the first frame pass instance (main scene with blur effect).
+
+    {
+        // Create the render index with the Storm render delegate.
+        hvt::RendererDescriptor renderDesc;
+        renderDesc.hgiDriver    = &context->_backend->hgiDriver();
+        renderDesc.rendererName = "HdStormRendererPlugin";
+        hvt::ViewportEngine::CreateRenderer(framePass1.renderIndex, renderDesc);
+
+        framePass1.sceneIndex = mergingSceneIndex;
+
+        framePass1.renderIndex->RenderIndex()->InsertSceneIndex(
+            framePass1.sceneIndex, SdfPath::AbsoluteRootPath());
+
+        // Create the frame pass instance.
+        hvt::FramePassDescriptor passDesc;
+        passDesc.renderIndex      = framePass1.renderIndex->RenderIndex();
+        passDesc.uid              = SdfPath("/sceneFramePass1");
+        framePass1.sceneFramePass = hvt::ViewportEngine::CreateFramePass(passDesc);
+
+        // Add a blur effect to the frame pass.
+
+        static constexpr float blurValue = 8.0f;
+        hvt::TaskManagerPtr& taskManager = framePass1.sceneFramePass->GetTaskManager();
+
+        auto fnCommit = [](hvt::TaskManager::GetTaskValueFn const& fnGetValue,
+                            hvt::TaskManager::SetTaskValueFn const& fnSetValue)
+        {
+            const VtValue value        = fnGetValue(HdTokens->params);
+            hvt::BlurTaskParams params = value.Get<hvt::BlurTaskParams>();
+            params.blurAmount          = blurValue;
+            fnSetValue(HdTokens->params, VtValue(params));
+        };
+
+        const SdfPath& insertPos = taskManager->GetTaskPath(HdxPrimitiveTokens->presentTask);
+        taskManager->AddTask<hvt::BlurTask>(hvt::BlurTask::GetToken(), hvt::BlurTaskParams(),
+            fnCommit, insertPos, hvt::TaskManager::InsertionOrder::insertBefore);
+    }
+
+    // Create the second frame pass instance (additional content in center quarter).
+
+    {
+        // Create the render index with the Storm render delegate.
+        hvt::RendererDescriptor renderDesc;
+        renderDesc.hgiDriver    = &context->_backend->hgiDriver();
+        renderDesc.rendererName = "HdStormRendererPlugin";
+        hvt::ViewportEngine::CreateRenderer(framePass2.renderIndex, renderDesc);
+
+        framePass2.sceneIndex = mergingSceneIndex;
+
+        framePass2.renderIndex->RenderIndex()->InsertSceneIndex(
+            framePass2.sceneIndex, SdfPath::AbsoluteRootPath());
+
+        // Create the frame pass instance.
+        hvt::FramePassDescriptor passDesc;
+        passDesc.renderIndex      = framePass2.renderIndex->RenderIndex();
+        passDesc.uid              = SdfPath("/sceneFramePass2");
+        framePass2.sceneFramePass = hvt::ViewportEngine::CreateFramePass(passDesc);
+    }
+
+    // Create the third frame pass instance (content displayed at bottom right).
+
+    {
+        // Create the render index with the Storm render delegate.
+        hvt::RendererDescriptor renderDesc;
+        renderDesc.hgiDriver    = &context->_backend->hgiDriver();
+        renderDesc.rendererName = "HdStormRendererPlugin";
+        hvt::ViewportEngine::CreateRenderer(framePass3.renderIndex, renderDesc);
+
+        framePass3.sceneIndex = mergingSceneIndex;
+
+        framePass3.renderIndex->RenderIndex()->InsertSceneIndex(
+            framePass3.sceneIndex, SdfPath::AbsoluteRootPath());
+
+        // Create the frame pass instance.
+        hvt::FramePassDescriptor passDesc;
+        passDesc.renderIndex      = framePass3.renderIndex->RenderIndex();
+        passDesc.uid              = SdfPath("/sceneFramePass3");
+        framePass3.sceneFramePass = hvt::ViewportEngine::CreateFramePass(passDesc);
+    }
+
+    const auto width  = context->width();
+    const auto height = context->height();
+
+    // Render loop.
+    int frameCount = 5;
+
+    auto render = [&]()
+    {
+        // First frame pass: full display with blur effect.
+
+        {
+            HD_TRACE_SCOPE("Three Frame Passes: Render Frame Pass 1");
+
+            auto& pass   = framePass1.sceneFramePass;
+            auto& params = pass->params();
+
+            params.renderBufferSize = GfVec2i(width, height);
+            params.viewInfo.framing = hvt::ViewParams::GetDefaultFraming(width, height);
+
+            params.viewInfo.viewMatrix       = stage.viewMatrix();
+            params.viewInfo.projectionMatrix = stage.projectionMatrix();
+            params.viewInfo.lights           = stage.defaultLights();
+            params.viewInfo.material         = stage.defaultMaterial();
+            params.viewInfo.ambient          = stage.defaultAmbient();
+
+            params.colorspace     = HdxColorCorrectionTokens->disabled;
+            params.selectionColor = TestHelpers::ColorYellow;
+
+            params.clearBackgroundColor = true;
+            params.backgroundColor      = TestHelpers::ColorDarkGrey;
+
+            params.clearBackgroundDepth = true;
+            params.backgroundDepth      = 1.0f;
+
+            // Do not present yet, wait for subsequent frame passes.
+            params.enablePresentation = false;
+
+            const HdTaskSharedPtrVector renderTasks = pass->GetRenderTasks();
+
+            pass->Render(renderTasks);
+
+            {
+                HD_TRACE_SCOPE("Three Frame Passes: waitForGPUIdle Frame Pass 1");
+                context->_backend->waitForGPUIdle();
+            }
+        }
+
+        // Get color & depth from the first frame pass to share the buffers.
+        hvt::RenderBufferBindings inputAOVs =
+            framePass1.sceneFramePass->GetRenderBufferBindingsForNextPass(
+                { HdAovTokens->color, HdAovTokens->depth });
+
+        // Second frame pass: display center quarter only.
+
+        {
+            HD_TRACE_SCOPE("Three Frame Passes: Render Frame Pass 2");
+
+            // Define center quarter framing.
+            const int quarterWidth  = width / 4;
+            const int quarterHeight = height / 4;
+            const int offsetX       = width / 3;
+            const int offsetY       = height / 3;
+
+            auto& pass   = framePass2.sceneFramePass;
+            auto& params = pass->params();
+
+            params.renderBufferSize = GfVec2i(width, height);
+            params.viewInfo.framing = {
+                { { 0, 0 }, { static_cast<float>(width), static_cast<float>(height) } },
+                { { offsetX, offsetY }, { offsetX + quarterWidth, offsetY + quarterHeight } }, 1.0f
+            };
+
+            params.viewInfo.viewMatrix       = stage.viewMatrix();
+            params.viewInfo.projectionMatrix = stage.projectionMatrix();
+            params.viewInfo.lights           = stage.defaultLights();
+            params.viewInfo.material         = stage.defaultMaterial();
+            params.viewInfo.ambient          = stage.defaultAmbient();
+
+            params.colorspace = HdxColorCorrectionTokens->disabled;
+
+            // Do not clear the background as it contains the first frame pass result.
+            params.clearBackgroundColor = false;
+            params.backgroundColor      = TestHelpers::ColorBlackNoAlpha;
+            params.selectionColor       = TestHelpers::ColorYellow;
+
+            params.clearBackgroundDepth = false;
+            params.backgroundDepth      = 1.0f;
+
+            // Do not present yet, wait for the third frame pass.
+            params.enablePresentation = false;
+
+            // Get render tasks with input AOVs and render.
+            const HdTaskSharedPtrVector renderTasks = pass->GetRenderTasks(inputAOVs);
+
+            pass->Render(renderTasks);
+
+            {
+                HD_TRACE_SCOPE("Three Frame Passes: waitForGPUIdle Frame Pass 2");
+                context->_backend->waitForGPUIdle();
+            }
+        }
+
+        // Get updated AOVs after second frame pass.
+        hvt::RenderBufferBindings inputAOVsForPass3 =
+            framePass2.sceneFramePass->GetRenderBufferBindingsForNextPass(
+                { HdAovTokens->color, HdAovTokens->depth });
+
+        // Third frame pass: display content at bottom right.
+
+        {
+            HD_TRACE_SCOPE("Three Frame Passes: Render Frame Pass 3");
+
+            // Define bottom right framing.
+            const int passWidth  = width / 4;
+            const int passHeight = height / 4;
+            const int offsetX    = width - passWidth - 10;
+            const int offsetY    = height - passHeight - 10;
+
+            auto& pass   = framePass3.sceneFramePass;
+            auto& params = pass->params();
+
+            params.renderBufferSize = GfVec2i(width, height);
+            params.viewInfo.framing =
+                hvt::ViewParams::GetDefaultFraming(offsetX, offsetY, passWidth, passHeight);
+
+            params.viewInfo.viewMatrix       = stage.viewMatrix();
+            params.viewInfo.projectionMatrix = stage.projectionMatrix();
+            params.viewInfo.lights           = stage.defaultLights();
+            params.viewInfo.material         = stage.defaultMaterial();
+            params.viewInfo.ambient          = stage.defaultAmbient();
+
+            params.colorspace = HdxColorCorrectionTokens->disabled;
+
+            // Do not clear the background color as it contains the previous frame passes
+            // result.
+            params.clearBackgroundColor = false;
+            params.backgroundColor      = TestHelpers::ColorBlackNoAlpha;
+            params.selectionColor       = TestHelpers::ColorYellow;
+
+            // Clear the depth buffer for this region so the content is visible.
+            params.clearBackgroundDepth = false;
+            params.backgroundDepth      = 1.0f;
+
+            // This is the final frame pass, enable presentation.
+            params.enablePresentation = context->presentationEnabled();
+
+            // Get render tasks with input AOVs and render.
+            const HdTaskSharedPtrVector renderTasks = pass->GetRenderTasks(inputAOVsForPass3);
+
+            pass->Render(renderTasks);
+
+            {
+                HD_TRACE_SCOPE("Three Frame Passes: waitForGPUIdle Frame Pass 3");
+                context->_backend->waitForGPUIdle();
+            }
+        }
+
+        return --frameCount > 0;
+    };
+
+    {
+        HD_TRACE_SCOPE("Three Frame Passes: Render");
+        context->run(render, framePass3.sceneFramePass.get());
+    }
+
+    // Validate the rendering result.
+
+    const std::string computedImagePath = TestHelpers::getComputedImagePath();
+    ASSERT_TRUE(context->validateImages(computedImagePath, TestHelpers::gTestNames.fixtureName));
+}
